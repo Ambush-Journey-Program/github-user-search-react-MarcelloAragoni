1aa138639eea00ba66bd1778afa9930e3e30bfd1
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2022-11-28 11:29:08 -0300

    created avatar component (#9)
    ## github-user-search-react-MarcelloAragoni
    
    ## Description
    created component avatar
    
    ## Screenshot
    <img width="238" alt="image" src="https://user-images.githubusercontent.com/32502447/204293749-460778d6-d0eb-46c8-a1a4-0ba3b7d34922.png">

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,11 @@
-import UserBox from "../../components/user-box/"
-import { handleFormatDate} from "../../utilities/format-date";
+import Avatar from "../../components/avatar/"
 
 export default function Home(){
-  const date = handleFormatDate('2018-03-13T12:20:43Z');
 
   return(
-    <UserBox
-      user='Marcello'
-      tag='marcello'
-      date={date}
-      bio='bio teste'
+    <Avatar
+      src='medusa.jpeg'
+      alt='user avatar'
     />
   );
 }
